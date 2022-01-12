5b73022da8b93f9b5d33e2fe1b0a45b561d25a1b
a_z <azdvd@mail.ru>
2022-01-12 13:50:42 +0300

    GB_Laravel_lesson_01

---
+++ 00/resources/views/homework01.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Урок 1</title>
+
+        <style>
+            body {
+                font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+                margin: 10px;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h4>Урок 1. Установка Laravel. Описание принципов работы и структуры фреймворка</h4>
+        <hr>
+        <ol>
+            <li>Настроить на локальной машине окружение для работы с фреймворком.</li>
+            <li>Ознакомиться с документацией.</li>
+            <li>Установить Laravel.</li>
+            <li>Реализовать несколько страниц с выводом какой-либо информации.</li>
+        </ol>
+        <hr>        
+    </body>
+</html>
